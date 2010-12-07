c24d2471314a75ed614edcfb42b78a32719ad280
Robert Lail <lailrc@a09-0016.cph.pri>
2010-12-07 14:07:08 -0600

    better code

--- assets/javascripts/freight_train/inline_editor.js
+++ assets/javascripts/freight_train/inline_editor.js
@@ -114,8 +114,9 @@ var InlineEditor = (function(){
   // Close InlineEditors when the ESC key is pressed
   document.observe('dom:loaded', function() {
     $(document.body).observe('keyup', function(event) {
-      if(event.keyCode==Event.KEY_ESC)
+      if(event.keyCode==Event.KEY_ESC) {
         InlineEditor.close();
+      }
     });
   });
   
