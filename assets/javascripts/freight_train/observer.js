695cd7def521d21e8627a4b667875b06abda3e1d
Bob Lail <bob.lailfamily@gmail.com>
2011-04-28 21:29:25 -0500

    refactored Observer to be Prototype-independent

--- assets/javascripts/freight_train/observer.js
+++ assets/javascripts/freight_train/observer.js
@@ -1,28 +1,35 @@
-/* Observer: this awesome class is described by The Grubbsian
-   http://www.thegrubbsian.com/?p=100 */
-/* modified to use prototype extensions */
-
+//
+// Observer
+//
+// Adapted from an example on The Grubbsian
+// http://www.thegrubbsian.com/
+//
 var Observer = (function(){
   var Observation = function(name, func) {
     this.name = name;
     this.func = func;
   };
   var constructor = function() {
-    //this.observations = [];
-    this.observations = new Array();
+    this.observations = [];
   };
+  function select(array, fn) {
+    var selected = [];
+    for(var i=0, ii=array.length, item; i<ii; i++) {
+      item = array[i];
+      fn(item) && selected.push(item);
+    }
+    return selected;
+  }
   constructor.prototype = {
     observe: function(name, func) {
-      var exists = this.observations.findAll(function(i) {
+      var exists = select(this.observations, function(i) {
         return (i.name==name) && (i.func==func);
       }).length > 0;
-      if(!exists) {
-        this.observations.push(new Observation(name, func));
-      }
+      !exists && this.observations.push(new Observation(name, func));
     },
     unobserve: function(name, func) {
-      for(var i=0; i<this.observations.length;) {
-        var observation = this.observations[i];
+      for(var i=0, ii=this.observations.length, observation; i<ii;) {
+        observation = this.observations[i];
         if((observation.name==name) && (observation.func==func)) {
           this.observations.splice(i, 1);
         } else {
@@ -31,11 +38,12 @@ var Observer = (function(){
       }
     },
     fire: function(name, data, scope) {
-      if(!(data instanceof Array)) data = [data];
-      var _observations = this.observations.findAll(function(i) { return (i.name==name); });
-      _observations.each(function(i) { i.func.apply(scope, data); });
-      //funcs.forEach(function(i) { i.func.apply(scope, data); });
+      !(data instanceof Array) && (data = [data]);
+      var _observations = select(this.observations, function(i) { return (i.name==name); });
+      for(var i=0, ii=_observations.length; i<ii; i++) {
+        _observations[i].func.apply(scope, data);
+      }
     }
   };
-  return constructor;    
+  return constructor;
 })();
