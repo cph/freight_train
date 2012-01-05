f5e3b83421ddccfd96cc74d36c3fc7a3cf4eec5e
Robert Lail <robert.lail@cph.org>
2012-01-05 09:03:17 -0600

    extend ActionDispatch::Request if Rails 3.1

--- lib/freight_train/extensions/request.rb
+++ lib/freight_train/extensions/request.rb
@@ -1,11 +1,10 @@
-class ActionController::Request
-  
-  
+module RequestExtension
   
   def freight_train?
     (parameters[:freight_train] == 'true')
   end
   
-  
-  
 end
+
+ActionController::Request.send(:include, RequestExtension) if defined?(ActionController::Request)
+ActionDispatch::Request.send(:include, RequestExtension) if defined?(ActionDispatch::Request)  
